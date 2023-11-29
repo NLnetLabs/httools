107723d5bc455b516c7b2abfacbabdd785717350
Martin Hoffmann <martin@nlnetlabs.nl>
2023-11-29 18:14:35 +0100

    Redesign RequestPath and RequestQuery.

--- src/request.rs
+++ src/request.rs
@@ -1,6 +1,6 @@
 //! Processing requests.
 
-use std::slice;
+use std::{mem, slice};
 use std::borrow::Cow;
 use std::collections::HashMap;
 use hyper::{Body, Uri};
@@ -26,7 +26,7 @@ impl Request {
         self.0.uri()
     }
 
-    pub fn path(&self) -> RequestPath {
+    pub fn path(&self) -> Result<RequestPath, InvalidPath> {
         RequestPath::from_request(self)
     }
 
@@ -63,92 +63,97 @@ impl From<hyper::Request<Body>> for Request {
 
 //------------ RequestPath ---------------------------------------------------
 
+#[derive(Debug)]
 pub struct RequestPath {
     path: Result<PathAndQuery, String>,
-    segment: (usize, usize),
 }
 
 impl RequestPath {
-    fn from_request(request: &Request) -> Self {
+    fn from_request(request: &Request) -> Result<Self, InvalidPath> {
         let path = if let Cow::Owned(some) = percent_decode(
             request.uri().path().as_bytes()
-        ).decode_utf8_lossy() {
+        ).decode_utf8().map_err(|_| InvalidPath)? {
             Err(some)
         }
         else {
-            Ok(request.uri().path_and_query().unwrap().clone())
+            Ok(request.uri().path_and_query().ok_or(InvalidPath)?.clone())
         };
-        let mut res = RequestPath {
-            path,
-            segment: (0, 0),
-        };
-        res.next_segment();
-        res
+        Ok(Self { path })
     }
 
-    pub fn full(&self) -> &str {
+    pub fn as_str(&self) -> &str {
         match self.path.as_ref() {
             Ok(path) => path.path(),
             Err(path) => path.as_str()
         }
     }
 
+    pub fn iter(&self) -> PathIter {
+        PathIter::new(self.as_str())
+    }
+}
+
+impl AsRef<str> for RequestPath {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+
+//------------ PathIter -------------------------------------------------
+
+#[derive(Debug)]
+pub struct PathIter<'a> {
+    full: &'a str,
+    remaining: &'a str,
+}
+
+impl<'a> PathIter<'a> {
+    fn new(path: &'a str) -> Self {
+        let remaining = if path.starts_with('/') {
+            &path[1..]
+        }
+        else {
+            path
+        };
+        Self { full: path, remaining }
+    }
+
+    pub fn full(&self) -> &str {
+        self.full
+    }
+
     pub fn remaining(&self) -> &str {
-        &self.full()[self.segment.0..]
+        self.remaining
     }
 
-    pub fn segment(&self) -> &str {
-        &self.full()[self.segment.0..self.segment.1]
+    pub fn len(&self) -> usize {
+        self.remaining.len()
     }
 
-    fn next_segment(&mut self) -> bool {
-        let mut start = self.segment.1;
-        let path = self.full();
-        // Start beyond the length of the path signals the end.
-        if start >= path.len() {
-            return false;
-        }
-        // Skip any leading slashes. There may be multiple which should be
-        // folded into one (or at least that’s what we do).
-        while path.split_at(start).1.starts_with('/') {
-            start += 1
-        }
-        // Find the next slash. If we have one, that’s the end of
-        // our segment, otherwise, we go all the way to the end of the path.
-        let end = path[start..].find('/').map(|x| x + start)
-                                         .unwrap_or(path.len());
-        self.segment = (start, end);
-        true 
+    pub fn is_empty(&self) -> bool {
+        self.remaining.is_empty()
     }
+}
 
-    pub fn next(&mut self) -> Option<&str> {
-        if self.next_segment() {
-            Some(self.segment())
-        }
-        else {
-            None
-        }
-    }
+impl<'a> Iterator for PathIter<'a> {
+    type Item = &'a str;
 
-    /// Returns the next segment if it is the final segment.
-    ///
-    /// If there are more segments after the next segment, returns the entire
-    /// remaining path as an error.
-    pub fn next_and_last(&mut self) -> Result<Option<&str>, &str> {
-        if !self.next_segment() {
-            return Ok(None)
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.remaining.is_empty() {
+            return None
         }
-        let path = self.full();
-        if self.segment.1 == path.len()
-            || (self.segment.1 + 1 == path.len()
-                    && path.as_bytes()[self.segment.1] == b'/'
-                )
-        {
-            return Ok(Some(self.segment()))
-        }
-        else {
-            Err(self.remaining())
-        }
+        let slash = match self.remaining.find('/') {
+            Some(pos) => pos,
+            None => {
+                let res = self.remaining;
+                self.remaining = "";
+                return Some(res)
+            }
+        };
+        let res = &self.remaining[..slash];
+        self.remaining = &self.remaining[slash + 1..];
+        Some(res)
     }
 }
 
@@ -157,14 +162,18 @@ impl RequestPath {
 
 #[derive(Default)]
 pub struct RequestQuery {
-    query: HashMap<String, Result<String, Vec<String>>>,
+    query: HashMap<String, QueryValue>,
 }
 
 impl RequestQuery {
     fn from_request(request: &Request) -> Self {
+        Self::from_uri(request.uri())
+    }
+
+    fn from_uri(uri: &Uri) -> Self {
         let mut res = Self::default();
         form_urlencoded::parse(
-            request.uri().query().unwrap_or("").as_bytes()
+            uri.query().unwrap_or("").as_bytes()
         ).into_owned().for_each(|(key, value)| { res.insert(key, value); });
         res
     }
@@ -174,31 +183,109 @@ impl RequestQuery {
 
         match self.query.entry(key) {
             Occupied(mut entry) => {
-                if let Err(entry) = entry.get_mut().as_mut() {
-                    entry.push(value);
-                }
-                else {
-                    *(entry.get_mut()) = Err(vec!(value));
-                }
+                entry.get_mut().into_multi().push(value)
             }
             Vacant(entry) => {
-                entry.insert(Ok(value));
+                entry.insert(QueryValue::Single(value));
             }
         }
     }
 
     pub fn get(&self, key: &str) -> Option<&[String]> {
-        match self.query.get(key).as_ref()? {
-            Ok(s) => Some(slice::from_ref(s)),
-            Err(vec) => Some(vec.as_ref()),
+        match self.query.get(key)? {
+            QueryValue::Single(s) => Some(slice::from_ref(s)),
+            QueryValue::Multi(vec) => Some(vec.as_ref()),
         }
     }
 
+    pub fn take(&mut self, key: &str) -> Option<QueryValue> {
+        self.query.remove(key)
+    }
+
     pub fn get_first(&self, key: &str) -> Option<&str> {
-        match self.query.get(key).as_ref()? {
-            Ok(s) => Some(s.as_str()),
-            Err(vec) => vec.first().map(String::as_str)
+        match self.query.get(key)? {
+            QueryValue::Single(s) => Some(s.as_str()),
+            QueryValue::Multi(vec) => vec.first().map(String::as_str)
         }
+    }
+}
+
+
+//------------ QueryValue ----------------------------------------------------
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum QueryValue {
+    Single(String),
+    Multi(Vec<String>),
+}
+
+impl QueryValue {
+    fn into_multi(&mut self) -> &mut Vec<String> {
+        if let Self::Multi(ref mut vec) = self {
+            return vec
+        }
+        let old = mem::replace(self, Self::Multi(Vec::new()));
+        let vec = match self {
+            Self::Multi(ref mut vec) => vec,
+            _ => unreachable!()
+        };
+        match old {
+            Self::Single(old) => vec.push(old),
+            _ => unreachable!()
+        }
+        vec
+    }
+}
+
+
+//------------ InvalidPath ---------------------------------------------------
+
+#[derive(Debug)]
+pub struct InvalidPath;
+
+
+//============ Tests =========================================================
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+    use super::*;
+
+    #[test]
+    fn request_query() {
+        let query = RequestQuery::from_uri(
+            &Uri::from_str("http://foo/bar?a=b&c=d&e=f&c=g").unwrap()
+        );
+        let mut vec = query.query.clone().into_iter().collect::<Vec<_>>();
+        vec.sort_by(|l, r| l.0.cmp(&r.0));
+
+        assert_eq!(
+            vec.as_slice(),
+            &[
+                ("a".into(), QueryValue::Single("b".into())),
+                ("c".into(), QueryValue::Multi(
+                    vec!["d".into(), "g".into()]
+                )),
+                ("e".into(), QueryValue::Single("f".into())),
+            ]
+        );
+
+        assert_eq!(
+            query.get("a"),
+            Some(["b".into()].as_slice())
+        );
+        assert_eq!(
+            query.get("c"),
+            Some(["d".into(), "g".into()].as_slice())
+        );
+        assert_eq!(
+            query.get("e"),
+            Some(["f".into()].as_slice())
+        );
+
+        assert_eq!(query.get_first("a"), Some("b"));
+        assert_eq!(query.get_first("c"), Some("d"));
+        assert_eq!(query.get_first("e"), Some("f"));
     }
 }
 
