b04086bbe0afced68fb0de4976f5d6cd7d6868e0
Martin Hoffmann <martin@nlnetlabs.nl>
2023-11-29 18:14:04 +0100

    Re-export hyper and ResponseBuilder.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! Tools for building web services with Hyper
 
+pub use hyper;
+
 pub use self::request::{Request, RequestPath};
-pub use self::response::Response;
+pub use self::response::{Response, ResponseBuilder};
 
 pub mod date;
 pub mod json;
